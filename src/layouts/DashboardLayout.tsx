1a1718088698e2e073708f5f2f3e1dd1ff760554
devdeepak06 <k.deepak2217@gmail.com>
2024-08-26 16:25:33 +0530

    add logout funcionality

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -32,10 +32,15 @@ import {
 } from "lucide-react";
 import { Link, Navigate, Outlet } from "react-router-dom";
 const DashboardLayout = () => {
-  const token = useTokenStore((state) => state.token);
+  const { token, setToken } = useTokenStore((state) => state);
+
   if (token === "") {
     return <Navigate to={"/auth/login"} replace />;
   }
+  const logout = () => {
+    console.log("Logging out");
+    setToken("");
+  };
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
@@ -194,7 +199,11 @@ const DashboardLayout = () => {
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button onClick={logout} variant={"ghost"}>
+                  Logout
+                </Button>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
