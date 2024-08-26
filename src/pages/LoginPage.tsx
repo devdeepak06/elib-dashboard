07e7b069289ce26263473bb44e1073f48d27a520
devdeepak06 <k.deepak2217@gmail.com>
2024-08-26 14:45:15 +0530

    add login state

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,6 +13,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "@/http/api";
+import { LoaderCircle } from "lucide-react";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -72,8 +73,13 @@ const LoginPage = () => {
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button onClick={handleLoginSubmit} className="w-full">
-              Sign in
+            <Button
+              onClick={handleLoginSubmit}
+              className="w-full"
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending && <LoaderCircle className="animate-spin" />}
+              <span className="ml-2">Sign in</span>
             </Button>
             <div className="mt-4 text-center text-sm">
               Don't have an account?{" "}
