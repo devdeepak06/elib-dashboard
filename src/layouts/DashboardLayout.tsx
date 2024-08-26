6578031ae9412cfdf2a90a17a0bc7171b22186c2
devdeepak06 <k.deepak2217@gmail.com>
2024-08-26 13:58:51 +0530

    add tanstack query

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -37,7 +37,7 @@ const DashboardLayout = () => {
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link
-              to="/dashboard/home"
+              to={"/dashboard/home"}
               className="flex items-center gap-2 font-semibold"
             >
               <Package2 className="h-6 w-6" />
@@ -51,7 +51,7 @@ const DashboardLayout = () => {
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                to="/dashboard/home"
+                to={"/dashboard/home"}
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
@@ -59,7 +59,7 @@ const DashboardLayout = () => {
               </Link>
 
               <Link
-                to="/dashboard/books"
+                to={"/dashboard/books"}
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Package className="h-4 w-4" />
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,9 +10,58 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log("Data", { email, password });
+    //make server call
+  };
   return (
+    // <section className="flex justify-center items-center h-screen">
+    //   <Card className="w-full max-w-sm">
+    //     <CardHeader>
+    //       <CardTitle className="text-2xl">Login</CardTitle>
+    //       <CardDescription>
+    //         Enter your email below to login to your account.
+    //       </CardDescription>
+    //     </CardHeader>
+    //     <CardContent className="grid gap-4">
+    //       <div className="grid gap-2">
+    //         <Label htmlFor="email">Email</Label>
+    //         <Input
+    //           ref={emailRef}
+    //           id="email"
+    //           type="email"
+    //           placeholder="xyz@example.com"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="grid gap-2">
+    //         <Label htmlFor="password">Password</Label>
+    //         <Input ref={passwordRef} id="password" type="password" required />
+    //       </div>
+    //     </CardContent>
+    //     <CardFooter>
+    //       <div className="w-full">
+    //         <Button onClick={handleLoginSubmit} className="w-full">
+    //           Sign in
+    //         </Button>
+    //         <div className="mt-4 text-center text-sm">
+    //           Don't have an account?{" "}
+    //           <Link to={"/auth/register"} className="underline">
+    //             Sign up
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </CardFooter>
+    //   </Card>
+    // </section>
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
         <CardHeader>
@@ -21,32 +70,44 @@ const LoginPage = () => {
             Enter your email below to login to your account.
           </CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="xyz@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <div className="w-full">
-            <Button className="w-full">Sign in</Button>
-            <div className="mt-4 text-center text-sm">
-              Don't have an account?{" "}
-              <Link to="/auth/register" className="underline">
-                Sign up
-              </Link>
+        <form onSubmit={handleLoginSubmit}>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                ref={emailRef}
+                id="email"
+                type="email"
+                placeholder="xyz@example.com"
+                required
+                autoComplete="email"
+              />
             </div>
-          </div>
-        </CardFooter>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                ref={passwordRef}
+                id="password"
+                type="password"
+                required
+                autoComplete="current-password"
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="w-full">
+              <Button type="submit" className="w-full">
+                Sign in
+              </Button>
+              <div className="mt-4 text-center text-sm">
+                Don't have an account?{" "}
+                <Link to={"/auth/register"} className="underline">
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </CardFooter>
+        </form>
       </Card>
     </section>
   );
